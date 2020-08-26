99e3db69d19feb11c5733a915b2b0124adadbe42
kavindasilva <kss.kavinda@gmail.com>
2020-08-26 19:48:50 +0530

    Item get/{id} and create new

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -8,7 +8,32 @@ use App\Item;
 class ItemController extends Controller
 {
     public function getAll(){
-        $students = Item::get()->toJson(JSON_PRETTY_PRINT);
-        return response($students, 200);
+        $items = Item::get()->toJson(JSON_PRETTY_PRINT);
+        return response($items, 200);
+    }
+
+    public function getById($id){
+        if (Item::where('id', $id)->exists()) {
+            $item = Item::where('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+            return response($item, 200);
+        }
+        else {
+            return response()->json([
+              "message" => "Student not found"
+            ], 404);
+        }
+    }
+
+    public function addNew(Request $request){
+        // var_dump($request);exit;
+        $item = new Item;
+        $item->name = $request->name;
+        $item->batch = $request->batch;
+        $item->price = $request->price;
+        $item->save();
+
+        return response()->json([
+            "id" => $item->id
+        ], 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('students', 'ItemController@getAll');
-Route::get('students/{id}', 'ItemController@getStudent');
-Route::post('students', 'ItemController@createStudent');
-Route::put('students/{id}', 'ItemController@updateStudent');
-Route::delete('students/{id}','ItemController@deleteStudent');
+Route::get('item', 'ItemController@getAll');
+Route::get('item/{id}', 'ItemController@getById');
+Route::post('item', 'ItemController@addNew');
+Route::put('item/{id}', 'ItemController@updateStudent');
+Route::delete('item/{id}','ItemController@deleteStudent');
