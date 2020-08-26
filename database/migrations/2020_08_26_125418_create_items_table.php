d32a652bda226878c5484e4f925e2a62f70d8702
kavindasilva <kss.kavinda@gmail.com>
2020-08-26 18:36:46 +0530

    table creation and migration

---
+++ database/migrations/2020_08_26_125418_create_items_table.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * To migrate only this table, run,
+ * php artisan migrate --path=database\migrations\2020_08_26_125418_create_items_table.php
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('batch');
+            $table->double('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
