9ff6e3b437bade4dc67abb04a12017b17cc09141
kavindasilva <kss.kavinda@gmail.com>
2020-08-30 19:19:33 +0530

    add new bill with items

--- app/BillItem.php
+++ app/BillItem.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class BillItem extends Model
 {
     //
+    protected $table = 'bill_items';
+
+    protected $fillable = ['item_id', 'bill_id', 'qty'];
+
+    public $timestamps = false;
 }
--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -19,6 +19,30 @@ class BillController extends Controller
         }
         return response($res, 200);
     }
+    /**
+    {
+        "id": 2,
+        "cashier": 1,
+        "total": 12,
+        "created_at": null,
+        "updated_at": null,
+        "items": [
+            {
+                "id": 1,
+                "name": "sample-db",
+                "batch": "x001",
+                "price": 100,
+                "created_at": null,
+                "updated_at": null,
+                "pivot": {
+                    "bill_id": 2,
+                    "item_id": 1,
+                    "qty": 1.2
+                }
+            }
+        ]
+    }
+     */
 
     public function getById($id){
         if (Bill::where('id', $id)->exists()) {
@@ -33,18 +57,29 @@ class BillController extends Controller
 
     public function addNew(Request $request){
         $bill = new Bill;
-        $bill->name = $request->name;
-        $bill->batch = $request->batch;
-        $bill->price = $request->price;
-        // (!$bill->save()){}
+        $bill->cashier = $request->cashier;
+        $bill->total = $request->total; // @TODO: make dynamic
+        // $bill->itemsa = $request->items;
 
         $res = $this->saveExtended($bill);
         if(true!==$res){
             return $res;
         }
 
+        foreach($request->items as $itm){
+            $b_item = new \App\BillItem;
+            $b_item->bill_id = $bill->id;
+            $b_item->item_id = $itm["item_id"];
+            $b_item->qty = $itm["qty"];
+
+            $res = $this->saveExtended($b_item);
+            if(true!==$res){
+                return $res;
+            }
+        }
+
         return response([
-            $bill
+            $bill->extendedResult()
         ], 201);
     }
 
