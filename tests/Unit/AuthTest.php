b295006e8f29a28bf4c5d3938f3eef7a135a7915
kavindasilva <kss.kavinda@gmail.com>
2020-09-02 22:02:36 +0530

    WIP test  logged in user data

--- tests/Unit/AuthTest.php
+++ tests/Unit/AuthTest.php
@@ -10,6 +10,8 @@ use Tests\TestCase;
 class AuthTest extends TestCase
 {
     protected $token = null;
+    protected $email = "k@admin.com";
+    protected $password = "k";
 
     /**
      * A basic test example.
@@ -22,6 +24,7 @@ class AuthTest extends TestCase
         $response->assertStatus(200);
     }
 
+
     /**
      * Test if user can login trough internal api.
      *
@@ -30,19 +33,55 @@ class AuthTest extends TestCase
     public function testLogin()
     {
         $payload = [
-            "email" => "k@admin.com",
-            "password" => "k"
+            "email" => $this->email,
+            "password" => $this->password
         ];
         $response = $this->postJson('api/login', $payload);
         // var_dump($response);
 
         $response->assertStatus(200)
+            ->assertHeader('Content-Type', 'application/json')
             ->assertJsonStructure(['access_token', 'token_type', 'expires_in'])
             ->assertJsonPath('token_type', 'bearer');
 
         $this->token = $response["access_token"];
-
+        var_dump($this->token);
         // $this->assertAuthenticated('api');
+    }
+
+
+    /**
+     * Test current logged user data.
+     *
+     * @return void
+     */
+    public function testLoggedUser()
+    {
+        $payload = [
+            "email" => $this->email,
+            "password" => $this->password
+        ];
+        $response = $this->get('api/me', [
+            // $this->getAuthHeader()
+            "Authorization" => "Bearer ".($this->token),
+            "Accept" => "application/json"
+        ]);
+        var_dump($response);
+
+        $response->assertStatus(200)
+            ->assertHeader('Content-Type', 'application/json');
+            // ->assertJsonStructure(['access_token', 'token_type', 'expires_in'])
+            // ->assertJsonPath('token_type', 'bearer');
+
+        // $this->token = $response["access_token"];
+        // $this->assertAuthenticated('api');
+    }
+
+
+    protected function getAuthHeader(){
+        return (object)[
+            "Authorization" => "Bearer ".'$this->token'
+        ];
     }
 
 }
