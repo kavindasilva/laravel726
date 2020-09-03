953469de21872416442abdc3688fc2e831b5f549
kavindasilva <kss.kavinda@gmail.com>
2020-09-03 21:33:37 +0530

    code cleaning

--- tests/Unit/AuthTest.php
+++ tests/Unit/AuthTest.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 use App\user;
-// use PHPUnit\Framework\TestCase;
 
 class AuthTest extends TestCase
 {
@@ -38,7 +37,6 @@ class AuthTest extends TestCase
             "password" => $this->password
         ];
         $response = $this->postJson('api/login', $payload);
-        // var_dump($response);
 
         $response->assertStatus(200)
             ->assertHeader('Content-Type', 'application/json')
@@ -46,8 +44,6 @@ class AuthTest extends TestCase
             ->assertJsonPath('token_type', 'bearer');
 
         $this->token = $response["access_token"];
-        // var_dump($this->token);
-        // $this->assertAuthenticated('api');
         return $this;
     }
 
@@ -59,32 +55,16 @@ class AuthTest extends TestCase
      */
     public function testLoggedUser()
     {
-        // $this->testLogin();
-        // var_dump($this->token);
         $this->token = $this->getTokenForUser($this->adminUser());
         $response = $this->get('api/me', [
-            // $this->getAuthHeader(),
             "Authorization" => "Bearer ".($this->token),
             "Accept" => "application/json"
         ]);
-        print_r($response);
-        // var_dump($response);
 
-        // $I = \UnitTester();
         $response->assertStatus(200)
             ->assertHeader('Content-Type', 'application/json')
             ->assertJsonStructure(['id', 'name', 'email', 'created_at', 'created_at'])
             ->assertJsonPath('email', $this->email);
-            // ->assertJsonPath('token_type', 'bearer'); assertInternalType
-            
-        // $this->assertTrue( $response->data, 'int' );
-            // "name": "k",
-            // "email": "k@admin.com",
-            // "created_at": "2020-09-01T14:33:44.000000Z",
-            // "updated_at": "2020-09-01T14:33:44.000000Z"
-
-        // $this->token = $response["access_token"];
-        // $this->assertAuthenticated('api');
     }
 
 
@@ -105,15 +85,6 @@ class AuthTest extends TestCase
         // $user = \App\User::generate('Test Admin', 'test-admin@example.com', self::AUTH_PASSWORD);
         // $user->assignRole(Role::findByName('admin'));
         // return $user;
-    }
-
-    /**
-     * @TODO: remove
-     */
-    protected function getAuthHeader(){
-        return [
-            "Authorization" => "Bearer ".($this->token)
-        ];
     }
 
 }
