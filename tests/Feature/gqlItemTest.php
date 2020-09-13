c0766df3d3ec8a5d5a4470d579c7a0ff5e8640ab
kavindasilva <kss.kavinda@gmail.com>
2020-09-13 11:08:57 +0530

    WIP item CRUD test

--- tests/Feature/gqlItemTest.php
+++ tests/Feature/gqlItemTest.php
@@ -80,16 +80,72 @@ class gqlitemTest extends TestCase
     public function testItemCrudTest()
     {
         $new_id = $this->testCreateItemTest();
+        $this->testReadExistingItemTest($new_id );
+        $this->testEditItemTest($new_id );
+        $this->testReadExistingItemTest($new_id );
         $this->testDeleteItemTest($new_id);
+        $this->testReadNonExistingItemTest($new_id );
+    }
+
+    protected function testReadExistingItemTest()
+    {
+        $response = $this->graphQL(/** @lang GraphQL */ '
+            {
+                user(id:'.$this->existing_user_id.') {
+                    id
+                    name
+                    email
+                }
+            }
+        ');
+        // var_dump($response->content());
+        // var_dump( $this->jsonStringToArr($response->content()) );
+        $response->assertStatus(200)
+            ->assertHeader('Content-Type', 'application/json')
+            ->assertJsonStructure([
+                'data' => [
+                    'user' => [
+                        'id',
+                        'email',
+                        'name',
+                    ]
+                ]
+            ])
+            ->assertJsonFragment(['id' => "$this->existing_user_id"], $this->existing_user_id);
+    }
+
+    protected function testReadNonExistingItemTest()
+    {
+        $response = $this->graphQL(/** @lang GraphQL */ '
+            {
+                user(id:857) {
+                    id
+                    name
+                    email
+                }
+            }
+        ');
+        $response->assertStatus(200)
+            // ->assertJsonPath('data', 'invalid_credentials')
+            ->assertJsonStructure([
+                'data' => [
+                    'user' => [
+                        
+                    ]
+                ]
+            ])
+        ;
+        // $response
+        // var_dump($response);
     }
     /**
      {
         "data": {
             "createItem": {
-            "id": "21",
-            "name": "1",
-            "batch": "1",
-            "price": "11"
+                "id": "21",
+                "name": "1",
+                "batch": "1",
+                "price": "11"
             }
         }
     }
@@ -99,42 +155,44 @@ class gqlitemTest extends TestCase
         $response = $this->graphQL(/** @lang GraphQL */ '
             mutation{
                 createItem(
-                  name: "gql-unit-test",
-                  batch: "unit-gql",
-                  price: 122.5
+                    name: "gql-unit-test",
+                    batch: "unit-gql",
+                    price: 122.5
                 ){
-                  id
-                  name
-                  batch
-                  price
+                    id
+                    name
+                    batch
+                    price
                 }
             }
         ');
         $response->assertStatus(200)
-            // ->assertJsonPath('data', 'invalid_credentials')
-            // ->assertJsonStructure([
-            //     'data' => [
-            //         'user' => [
-                        
-            //         ]
-            //     ]
-            // ])
+            ->assertJsonStructure([
+                'data' => [
+                    'deleteItem' => [
+                        'id',
+                        'name',
+                        'batch',
+                        'price',
+                    ]
+                ]
+            ])
         ;
         // $response
         // var_dump($response);
-        $this->created_item_id = $this->jsonStringToArr($response->content())["data"]["createItem"]["id"];
+        // $this->created_item_id = $this->jsonStringToArr($response->content())["data"]["createItem"]["id"];
         return $this->jsonStringToArr($response->content())["data"]["createItem"]["id"];
-        var_dump($this->created_item_id);
+        // var_dump($this->created_item_id);
     }
 
     /**
     {
         "data": {
             "deleteItem": {
-            "id": "21",
-            "name": "1",
-            "batch": "1",
-            "price": "11"
+                "id": "21",
+                "name": "1",
+                "batch": "1",
+                "price": "11"
             }
         }
     }
@@ -146,25 +204,27 @@ class gqlitemTest extends TestCase
         $response = $this->graphQL(/** @lang GraphQL */ '
             mutation{
                 deleteItem(
-                  id: '.$ii.'
+                    id: '.$ii.'
                 ){
-                  id
-                  name
-                  batch
-                  price
+                    id
+                    name
+                    batch
+                    price
                 }
             }
         ');
-        $response->assertStatus(200);
-        //     // ->assertJsonPath('data', 'invalid_credentials')
-        //     ->assertJsonStructure([
-        //         'data' => [
-        //             'user' => [
-                        
-        //             ]
-        //         ]
-        //     ])
-        // ;
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'deleteItem' => [
+                        'id',
+                        'name',
+                        'batch',
+                        'price',
+                    ]
+                ]
+            ])
+        ;
         // $response
         // var_dump($response);
     }
